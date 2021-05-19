5ffff0ec40861070d00bc58b6dfe30236aa058d3
yedidya <yedidya@bazaart.me>
2021-05-19 14:28:12 +0300

    Initial Commit

---
+++ BazaartProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BazaartProject
+//
+//  Created by Yedidya Reiss on 19/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
